787bfc6d4ad85ae70051806c369f53cff5dd7ceb
caiohasouza <caio@ganex.com.br>
2023-03-01 15:27:50 -0300

    fix: add server_side_encryption_configuration to s3 bucket

--- main.tf
+++ main.tf
@@ -98,6 +98,13 @@ resource "aws_s3_bucket" "this" {
   bucket = "${var.name}-${data.aws_region.current.name}"
   acl    = "private"
   tags   = var.tags
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
